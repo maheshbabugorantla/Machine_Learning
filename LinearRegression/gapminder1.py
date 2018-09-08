59c41787d69c5bf43c954b263dc0441ffe5ead18
Mahesh Babu Gorantla <mgorantla@statestreet.com>
2018-09-08 17:39:36 -0500

    Added Linear Regression first exercise code

---
+++ LinearRegression/gapminder1.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import LinearRegression
+from pandas import read_csv, Series
+from numpy import array
+
+# Assign the dataframe to this variable.
+# TODO: Load the data
+bmi_life_data = read_csv('bmi_and_life_expectancy.csv')
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
+linear_reg_model = LinearRegression()
+X = array(bmi_life_data[['BMI']]).reshape(1, -1)
+y = array(bmi_life_data[['Life Expectancy']]).reshape(1, -1)
+bmi_life_model = linear_reg_model.fit(X, y)
+
+
+# Make a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+laos_life_exp = bmi_life_model.predict(array([21.07931]).reshape(1, -1))
+print(laos_life_exp)
