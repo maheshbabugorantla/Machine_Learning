f41d89fc6001c407ea498e6d20156dc9d6cb9838
Mahesh Babu Gorantla <mgorantla@statestreet.com>
2018-09-17 08:29:14 -0500

    Add something

--- LinearRegression/gapminder1.py
+++ LinearRegression/gapminder1.py
@@ -9,12 +9,14 @@ bmi_life_data = read_csv('bmi_and_life_expectancy.csv')
 # Make and fit the linear regression model
 #TODO: Fit the model and Assign it to bmi_life_model
 linear_reg_model = LinearRegression()
-X = array(bmi_life_data[['BMI']]).reshape(1, -1)
-y = array(bmi_life_data[['Life Expectancy']]).reshape(1, -1)
-bmi_life_model = linear_reg_model.fit(X, y)
 
+X = bmi_life_data[['BMI']]
+y = bmi_life_data['Life Expectancy']
+
+bmi_life_model = linear_reg_model.fit(X, y)
 
 # Make a prediction using the model
 # TODO: Predict life expectancy for a BMI value of 21.07931
 laos_life_exp = bmi_life_model.predict([[21.07931]])
 print(laos_life_exp)
+
